8e38ee88efa48490d25b20c33db78ca2e48cfd30
ArielQ1 <quizayaariel@gmail.com>
2024-04-01 17:51:23 -0400

    add pedidos

--- 08clase/restaurante/restaurante.py
+++ 08clase/restaurante/restaurante.py
@@ -33,77 +33,49 @@ conn.execute(
     """
 )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Insertar datos de platos
 conn.execute(
     """
     INSERT INTO PLATOS (nombre, precio, categoria) 
-    VALUES ('PIZZA', 10.99, 'Italiana')
+    VALUES ('PIZZA', 10.99, 'Italiana'), ('HAMBURGUESA', 8.99, 'Americana'), 
+            ('SUCHI', 12.99, 'Japonesa'),('ENSALADA', 10.99, 'Vegetariana')
     """
 )
-conn.execute(
-    """
-    INSERT INTO PLATOS (nombre, precio, categoria) 
-    VALUES ('HAMBURGUESA', 8.99, 'Americana')
-    """
-)
-conn.execute(
-    """
-    INSERT INTO PLATOS (nombre, precio, categoria) 
-    VALUES ('SUCHI', 12.99, 'Japonesa')
-    """
-)
-conn.execute(
-    """
-    INSERT INTO PLATOS (nombre, precio, categoria) 
-    VALUES ('ENSALADA', 10.99, 'Vegetariana')
-    """
-)
+
 # Insertar datos de mesas 
 conn.execute(
     """
     INSERT INTO MESAS (numero) 
-    VALUES (1)
+    VALUES (1),(2),(3),(4)
     """
 )
+#Insertar datos Pedidos
 conn.execute(
     """
-    INSERT INTO MESAS (numero) 
-    VALUES (2)
-    """
-)
-conn.execute(
-    """
-    INSERT INTO MESAS (numero) 
-    VALUES (3)
-    """
-)
-conn.execute(
-    """
-    INSERT INTO MESAS (numero) 
-    VALUES (4)
+    INSERT INTO PEDIDOS (plato_id, mesa_id, cantidad, fecha) 
+    VALUES 
+    (1, 2, 2, '2024-04-01'),
+    (2, 3, 1, '2024-04-01'),
+    (3, 1, 3, '2024-04-02'),
+    (4, 4, 1, '2024-04-02')
     """
 )
 
 # Consultar datos
-#print("PLATOS:")
-#cursor = conn.execute("SELECT * FROM PLATOS")
-#for row in cursor:
-#    print(row)
-
-
+print("\nPlatos:")
+cursor = conn.execute("SELECT * FROM PLATOS")
+for row in cursor:
+    print(row)
+# Consultar datos
+print("\nMesas:")
+cursor = conn.execute("SELECT * FROM MESAS")
+for row in cursor:
+    print(row)
+#Consultar datos 
+print("\nPedidos")
+cursor = conn.execute("SELECT * FROM PEDIDOS")
+for row in cursor:
+    print(row)
 
 
 
